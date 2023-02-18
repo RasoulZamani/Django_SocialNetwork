b8f6f994787afc5f854940334d91469a7bdc2152
RasoulZamani <r.zamani72@gmail.com>
2023-02-18 18:14:47 +0330

    adding basic templates

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is Home Page!")    
+    return render(request, 'home/index.html')#HttpResponse("This is Home Page!")    
