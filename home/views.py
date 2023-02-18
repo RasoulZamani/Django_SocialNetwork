6d582d7e81dd1828427b6bb1e9e56a83c5290cdf
RasoulZamani <r.zamani72@gmail.com>
2023-02-18 18:44:42 +0330

    class base view for homepage

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views import View 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home/index.html')#HttpResponse("This is Home Page!")    
+#def home(request):
+#    return render(request, 'home/index.html')#HttpResponse("This is Home Page!")
+class HomeView(View):
+    """CBV for home page"""
+    def get(self, request):
+        return render(request, 'home/index.html') 
+    
+    def post(self, request):
+        return render(request, 'home/index.html')           
