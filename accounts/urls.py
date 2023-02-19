0c05d8d5d7414ab27f5182c5c7e9e78b93919313
RasoulZamani <r.zamani72@gmail.com>
2023-02-19 11:48:45 +0330

    url name space for home and accounts apps

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name='accounts'
 urlpatterns = [
     #path('', views.AccountView.as_view(), name='accounts'),
     path('register/', views.RegisterView.as_view(), name='user_register'),
