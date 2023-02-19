4eba751719f12b223189292926455d4d6591a841
RasoulZamani <r.zamani72@gmail.com>
2023-02-19 12:13:12 +0330

    registerring user and show messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View 
 from .forms import UserRegisterForm
+from django.contrib.auth.models import User 
+from django.contrib import messages
+
 
 # Create your views here.
 class RegisterView(View):
@@ -11,4 +14,10 @@ class RegisterView(View):
         return render(request, 'accounts/register.html', {'user_form':user_form}) 
     
     def post(self, request):
-        return render(request, 'accounts/register.html')    
+        user_form = UserRegisterForm(request.POST)
+        if user_form.is_valid():
+            cd = user_form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, f"Hi Dear {cd['username']} ! Your account was created successfuly!",'success')
+        
+        return redirect('home:home')
