5445c4a4fef18c2226082bbb91ed74f590de1861
RasoulZamani <r.zamani72@gmail.com>
2023-02-19 15:51:20 +0330

    validating uniqness of email and username

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,27 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class UserRegisterForm(forms.Form):
     """from for user registeration"""
     username = forms.CharField(max_length=64, required=True,
-                               widget=forms.TextInput(attrs={'calss':'form-control'}))
+        widget=forms.TextInput(attrs={'calss':'form-control'}))
     email    = forms.EmailField(widget=forms.EmailInput(attrs={'calss':'form-control', 'placeholder':'example@mail.com'}))
-    password = forms.CharField(max_length=32, required=True,
-                               widget=forms.PasswordInput(attrs={'calss':'form-control', 'placeholder':'more than 8 character'}))
+    password = forms.CharField(max_length=32, min_length=8, required=True,
+        widget=forms.PasswordInput(attrs={'calss':'form-control', 'placeholder':'more than 8 character'}))
+    
+    def clean_email(self):
+        """checkimg uniqeness of email"""
+        input_email = self.cleaned_data['email']
+        user_with_email = User.objects.filter(email=input_email).exists()
+        if user_with_email:
+            raise ValidationError("This email is already exist!")
+        return input_email
+    
+    def clean_username(self):
+        """checkimg uniqeness of username"""
+        input_username = self.cleaned_data['username']
+        user_with_username = User.objects.filter(username=input_username).exists()
+        if user_with_username:
+            raise ValidationError("This username is already exist!")
+        return input_username
--- accounts/views.py
+++ accounts/views.py
@@ -8,16 +8,22 @@ from django.contrib import messages
 # Create your views here.
 class RegisterView(View):
     """CBV for Register"""
+    
+    form_class     = UserRegisterForm
+    template_class = 'accounts/register.html'
+    
     def get(self, request):
-        user_form = UserRegisterForm()
+        user_form = self.form_class()
         
-        return render(request, 'accounts/register.html', {'user_form':user_form}) 
+        return render(request, self.template_class, {'user_form':user_form}) 
     
     def post(self, request):
-        user_form = UserRegisterForm(request.POST)
+        user_form = self.form_class(request.POST)
         if user_form.is_valid():
             cd = user_form.cleaned_data
             User.objects.create_user(cd['username'], cd['email'], cd['password'])
             messages.success(request, f"Hi Dear {cd['username']} ! Your account was created successfuly!",'success')
         
-        return redirect('home:home')
+            return redirect('home:home')
+        
+        return render(request, self.template_class, {'user_form':user_form})
