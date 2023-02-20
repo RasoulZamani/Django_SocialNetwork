1d18b74bfbc6fa1be8223a4eba176572c725388c
RasoulZamani <r.zamani72@gmail.com>
2023-02-20 15:17:25 +0330

    btn for deleting posts

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 #from django.contrib.auth.models import User  
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
+
 #def home(request):
 #    return render(request, 'home/index.html')#HttpResponse("This is Home Page!")
 class HomeView(View):
@@ -17,3 +21,16 @@ class PostDetailView(View):
     def get(self, request, post_id, post_slug):
         post = Post.objects.get(pk=post_id, slug=post_slug)
         return render(request, 'home/details.html', {'post':post})
+    
+   
+class PostDeleteView(LoginRequiredMixin, View):
+    """CBV for deleting post """
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id :
+            post.delete()
+            messages.success(request, 'Your post was deleted seccessfuly!', 'seccess')
+        else:
+            messages.error(request, 'You can not delete other posts!!', 'error')
+            
+        return redirect('home:home')
