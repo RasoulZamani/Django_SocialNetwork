f5830e1fe23ad80204527d69786994843723c0d8
RasoulZamani <r.zamani72@gmail.com>
2023-02-26 11:04:25 +0330

    commenting

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from .forms import PostCreateUpdateForm
+from .forms import PostCreateUpdateForm, CommentCreateForm
 from django.utils.text import slugify
 
 
@@ -20,12 +20,27 @@ class HomeView(View):
     
 class PostDetailView(View):
     """CBV for post details"""
-    def get(self, request, post_id, post_slug):
-        post = Post.objects.get(pk=post_id, slug=post_slug)
+    class_form = CommentCreateForm
+    
+    def setup(self, request, *args, **kwargs):
+        self.post_inst = Post.objects.get(pk=kwargs['post_id'], slug=kwargs['post_slug'])
+        return super().setup(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        post = self.post_inst
         comments = post.post_comment.filter(is_reply=False)
         return render(request, 'home/details.html',
-                      {'post':post, 'comments':comments})
+                      {'post':post, 'comments':comments, 'comment_form':self.class_form})
     
+    def post(self, request, *args, **kwargs):
+        comment_form = self.class_form(request.POST)
+        if comment_form.is_valid():
+            new_commetn = comment_form.save(commit=False)
+            new_commetn.user = request.user
+            new_commetn.post = self.post_inst
+            new_commetn.save()
+            messages.success(request, 'Your comment was sent seccessfuly!', 'seccess')
+            return redirect('home:post_details', self.post_inst.id, self.post_inst.slug)
    
 class PostDeleteView(LoginRequiredMixin, View):
     """CBV for deleting post """
